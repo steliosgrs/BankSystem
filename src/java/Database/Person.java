fbabe9dd97dd874eecb9ebf7e322da85ce8cf81c
NoLiferX <steliosgp13@gmail.com>
2022-02-27 02:27:19 +0200

    Fix all

--- src/java/Database/Person.java
+++ src/java/Database/Person.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Database;
 public class Person {
     private int id,number;
@@ -65,71 +64,3 @@ public class Person {
     
     
 }
-=======
-package Database;
-public class Person {
-    private int id,number;
-    private String name,address,mail;
-    private float balance;
-    private boolean active;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public float getBalance() {
-        return balance;
-    }
-
-    public void setBalance(float balance) {
-        this.balance = balance;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-    
-    
-    
-}
->>>>>>> cc9054e7c13b6d3ccb9d874c2f3d96d8ffb80772
--- src/java/Servlets/DeleteServlet2.java
+++ src/java/Servlets/DeleteServlet2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package Servlets;
 import Database.*;
@@ -68,74 +67,3 @@ public class DeleteServlet2 extends HttpServlet {
     }// </editor-fold>
 
 }
-=======
-
-package Servlets;
-import Database.*;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author NoLifer
- */
-public class DeleteServlet2 extends HttpServlet {
-
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-        response.setContentType("text/html;charset=UTF-8");
-        
-        String id_s = request.getParameter("id");
-        int id = Integer.parseInt(id_s);
-        int status = 0;
-        status =PersonDao.deleteAcount(id);
-        
-        try (PrintWriter out = response.getWriter()) {
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Διαγραφή λογαριασμού</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DeleteServlet2 at " + request.getContextPath() + "</h1>");
-
-            
-            if (status == 0 ){
-                out.println("<h2>Account not Deleted</h2>");
-            }else {
-                out.println("<h2>Account Deleted Successfully</h2>");
-            }
-            
-            out.println("<br>");out.println("<br>");
-            out.println("<a href=\"index.html\">Μενού</a>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
->>>>>>> cc9054e7c13b6d3ccb9d874c2f3d96d8ffb80772
